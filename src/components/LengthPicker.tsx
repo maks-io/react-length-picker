0acd04e5763651e21d3ed99c9b0972ab75d0b9a9
Markus Kurzmann <markus@kurzmann.io>
2024-06-28 20:32:31 +0200

    Remove faulty memoization

--- src/components/LengthPicker.tsx
+++ src/components/LengthPicker.tsx
@@ -6,131 +6,130 @@ import { FeetAndInches } from "$/types/FeetAndInches";
 const fontSizePrimary = 18;
 const fontSizeSecondary = 9;
 
-export const LengthPicker = memo(
-  ({
-    containerWidth = 160,
-    containerHeight = 80,
-    entryHeight = 40,
-    defaultLength = 350,
-    unit = "metric",
-    onLengthChange = () => {},
-    onUnitChange = () => {},
-    metricStep = 1,
-    metricMin = 300,
-    metricMax = 400,
-    metricFormatter = (
-      isPrimary: boolean,
-      centimeters: number,
-      index: number,
-    ) => (
-      <>
-        <span
-          style={{
-            fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
-            fontWeight: "bold",
-          }}
-        >
-          {isPrimary ? centimeters : `~${Math.round(centimeters)}`}
-        </span>
-        <span
-          style={{
-            marginLeft: 2,
-            fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
-            fontWeight: "normal",
-          }}
-        >
-          <small>cm</small>
-        </span>
-      </>
-    ),
-    imperialStep = 1,
-    imperialMin = 118,
-    imperialMax = 157,
-    imperialFormatter = (
-      isPrimary: boolean,
-      inchesOnly: number,
-      feetAndInches: FeetAndInches,
-    ) => (
-      <>
-        <span
-          style={{
-            fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
-            fontWeight: "bold",
-          }}
-        >
-          {isPrimary ? feetAndInches.feet : `~${feetAndInches.feet}`}
-        </span>
-        <span
-          style={{
-            fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
-            fontWeight: "normal",
-          }}
-        >
-          ′
-        </span>
-        <span
-          style={{
-            fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
-            fontWeight: "bold",
-          }}
-        >
-          {isPrimary ? feetAndInches.inches : Math.round(feetAndInches.inches)}
-        </span>
-        <span
-          style={{
-            fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
-            fontWeight: "normal",
-          }}
-        >
-          ″
-        </span>
-      </>
-    ),
-    containerStyle,
-    entryContainerStyle,
-    entryContentStyle,
-    ascending = true,
-  }: ReactLengthPickerProps) => {
-    const discoverArea = entryHeight * 2 - 1;
-
-    let rootMargin: string;
-
-    if (discoverArea < containerHeight) {
-      const value = (containerHeight - discoverArea) / 2;
-      rootMargin = `-${value}px 0%`;
-    } else {
-      rootMargin = "0px";
-    }
-
-    return (
-      <div
+export const LengthPicker = ({
+  containerWidth = 160,
+  containerHeight = 80,
+  entryHeight = 40,
+  defaultLength = 350,
+  unit = "metric",
+  onLengthChange = () => {},
+  onUnitChange = () => {},
+  metricStep = 1,
+  metricMin = 300,
+  metricMax = 400,
+  metricFormatter = (
+    isPrimary: boolean,
+    centimeters: number,
+    index: number,
+  ) => (
+    <>
+      <span
         style={{
-          height: containerHeight,
-          width: containerWidth,
+          fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
+          fontWeight: "bold",
         }}
       >
-        <List
-          containerStyle={containerStyle}
-          unit={unit}
-          containerHeight={containerHeight}
-          entryHeight={entryHeight}
-          rootMargin={rootMargin}
-          metricStep={metricStep}
-          metricMin={metricMin}
-          metricMax={metricMax}
-          imperialStep={imperialStep}
-          imperialMin={imperialMin}
-          imperialMax={imperialMax}
-          metricFormatter={metricFormatter}
-          imperialFormatter={imperialFormatter}
-          defaultLength={defaultLength}
-          onLengthChange={onLengthChange}
-          entryContainerStyle={entryContainerStyle}
-          entryContentStyle={entryContentStyle}
-          ascending={ascending}
-          onUnitChange={onUnitChange}
-        />
-        <style>{`
+        {isPrimary ? centimeters : `~${Math.round(centimeters)}`}
+      </span>
+      <span
+        style={{
+          marginLeft: 2,
+          fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
+          fontWeight: "normal",
+        }}
+      >
+        <small>cm</small>
+      </span>
+    </>
+  ),
+  imperialStep = 1,
+  imperialMin = 118,
+  imperialMax = 157,
+  imperialFormatter = (
+    isPrimary: boolean,
+    inchesOnly: number,
+    feetAndInches: FeetAndInches,
+  ) => (
+    <>
+      <span
+        style={{
+          fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
+          fontWeight: "bold",
+        }}
+      >
+        {isPrimary ? feetAndInches.feet : `~${feetAndInches.feet}`}
+      </span>
+      <span
+        style={{
+          fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
+          fontWeight: "normal",
+        }}
+      >
+        ′
+      </span>
+      <span
+        style={{
+          fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
+          fontWeight: "bold",
+        }}
+      >
+        {isPrimary ? feetAndInches.inches : Math.round(feetAndInches.inches)}
+      </span>
+      <span
+        style={{
+          fontSize: isPrimary ? fontSizePrimary : fontSizeSecondary,
+          fontWeight: "normal",
+        }}
+      >
+        ″
+      </span>
+    </>
+  ),
+  containerStyle,
+  entryContainerStyle,
+  entryContentStyle,
+  ascending = true,
+}: ReactLengthPickerProps) => {
+  const discoverArea = entryHeight * 2 - 1;
+
+  let rootMargin: string;
+
+  if (discoverArea < containerHeight) {
+    const value = (containerHeight - discoverArea) / 2;
+    rootMargin = `-${value}px 0%`;
+  } else {
+    rootMargin = "0px";
+  }
+
+  return (
+    <div
+      style={{
+        height: containerHeight,
+        width: containerWidth,
+      }}
+    >
+      <List
+        containerStyle={containerStyle}
+        unit={unit}
+        containerHeight={containerHeight}
+        entryHeight={entryHeight}
+        rootMargin={rootMargin}
+        metricStep={metricStep}
+        metricMin={metricMin}
+        metricMax={metricMax}
+        imperialStep={imperialStep}
+        imperialMin={imperialMin}
+        imperialMax={imperialMax}
+        metricFormatter={metricFormatter}
+        imperialFormatter={imperialFormatter}
+        defaultLength={defaultLength}
+        onLengthChange={onLengthChange}
+        entryContainerStyle={entryContainerStyle}
+        entryContentStyle={entryContentStyle}
+        ascending={ascending}
+        onUnitChange={onUnitChange}
+      />
+      <style>{`
             .noselect {
             -webkit-touch-callout: none; /* iOS Safari */
             -webkit-user-select: none; /* Safari */
@@ -141,10 +140,6 @@ export const LengthPicker = memo(
                                   supported by Chrome, Edge, Opera and Firefox */
             }
             `}</style>
-      </div>
-    );
-  },
-  (prevProps, nextProps) => {
-    return JSON.stringify(prevProps) === JSON.stringify(nextProps);
-  },
-);
+    </div>
+  );
+};
--- src/components/ListEntry.tsx
+++ src/components/ListEntry.tsx
@@ -6,120 +6,111 @@ import { SecondaryListEntry } from "$/components/SecondaryListEntry";
 import { imperialToMetric } from "$/convert/imperialToMetric";
 import { ListEntryProps } from "$/types/ListEntryProps";
 
-export const ListEntry = memo(
-  ({
-    value,
-    parentRef,
-    entryHeight,
+export const ListEntry = ({
+  value,
+  parentRef,
+  entryHeight,
+  rootMargin,
+  setCurrentLengths,
+  currentLengths,
+  metricFormatter,
+  isFirst,
+  isLast,
+  containerHeight,
+  imperialFormatter,
+  unit,
+  index,
+  entryContainerStyle = {},
+  entryContentStyle = {},
+}: ListEntryProps) => {
+  const [ref, inView] = useInView({
     rootMargin,
-    setCurrentLengths,
-    currentLengths,
-    metricFormatter,
-    isFirst,
-    isLast,
-    containerHeight,
-    imperialFormatter,
-    unit,
-    index,
-    entryContainerStyle = {},
-    entryContentStyle = {},
-  }: ListEntryProps) => {
-    const [ref, inView] = useInView({
-      rootMargin,
-      threshold: 1,
-      root: parentRef.current,
-    });
+    threshold: 1,
+    root: parentRef.current,
+  });
 
-    useEffect(() => {
-      if (inView && !currentLengths.includes(value)) {
-        setCurrentLengths((prevState) => [...prevState, value]);
-      } else if (!inView && currentLengths.includes(value)) {
-        setCurrentLengths((prevState) => prevState.filter((v) => v !== value));
-      }
-      return () => {
-        setCurrentLengths((prevState) => prevState.filter((v) => v !== value));
-      };
-    }, [inView]);
+  useEffect(() => {
+    if (inView && !currentLengths.includes(value)) {
+      setCurrentLengths((prevState) => [...prevState, value]);
+    } else if (!inView && currentLengths.includes(value)) {
+      setCurrentLengths((prevState) => prevState.filter((v) => v !== value));
+    }
+    return () => {
+      setCurrentLengths((prevState) => prevState.filter((v) => v !== value));
+    };
+  }, [inView]);
 
-    const metricValue = useMemo(
-      () => (unit === "metric" ? value : imperialToMetric(value)),
-      [unit, value],
+  const metricValue = useMemo(
+    () => (unit === "metric" ? value : imperialToMetric(value)),
+    [unit, value],
+  );
+
+  const metricValueFormatted = useMemo(() => {
+    return metricFormatter(unit === "metric", metricValue, index);
+  }, [unit, metricFormatter, index, metricValue]);
+
+  const imperialValue = useMemo(
+    () => (unit === "imperial" ? value : metricToImperial(value)),
+    [unit, value],
+  );
+
+  const imperialValueFormatted = useMemo(() => {
+    const feetAndInches = inchesToFeetAndInches(imperialValue, true);
+    return imperialFormatter(
+      unit === "imperial",
+      imperialValue,
+      feetAndInches,
+      index,
     );
+  }, [unit, imperialFormatter, index, imperialValue]);
 
-    const metricValueFormatted = useMemo(() => {
-      return metricFormatter(unit === "metric", metricValue, index);
-    }, [unit, metricFormatter, index, metricValue]);
+  const entryContainerStyleEffective =
+    typeof entryContainerStyle === "function"
+      ? entryContainerStyle(index, metricValue, imperialValue, inView)
+      : entryContainerStyle;
 
-    const imperialValue = useMemo(
-      () => (unit === "imperial" ? value : metricToImperial(value)),
-      [unit, value],
-    );
+  const entryContentStyleEffective =
+    typeof entryContentStyle === "function"
+      ? entryContentStyle(index, metricValue, imperialValue, inView)
+      : entryContentStyle;
 
-    const imperialValueFormatted = useMemo(() => {
-      const feetAndInches = inchesToFeetAndInches(imperialValue, true);
-      return imperialFormatter(
-        unit === "imperial",
-        imperialValue,
-        feetAndInches,
-        index,
-      );
-    }, [unit, imperialFormatter, index, imperialValue]);
-
-    const entryContainerStyleEffective =
-      typeof entryContainerStyle === "function"
-        ? entryContainerStyle(index, metricValue, imperialValue, inView)
-        : entryContainerStyle;
-
-    const entryContentStyleEffective =
-      typeof entryContentStyle === "function"
-        ? entryContentStyle(index, metricValue, imperialValue, inView)
-        : entryContentStyle;
-
-    return (
+  return (
+    <div
+      className={"noselect"}
+      ref={ref}
+      style={{
+        position: "relative",
+        minHeight: entryHeight,
+        fontSize: 24,
+        fontWeight: "bold",
+        boxSizing: "border-box",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        scrollSnapAlign: "center",
+        ...entryContainerStyleEffective,
+        marginTop: isFirst ? containerHeight / 2 - entryHeight / 2 : undefined,
+        marginBottom: isLast
+          ? containerHeight / 2 - entryHeight / 2
+          : undefined,
+      }}
+    >
       <div
-        className={"noselect"}
-        ref={ref}
         style={{
-          position: "relative",
-          minHeight: entryHeight,
-          fontSize: 24,
-          fontWeight: "bold",
-          boxSizing: "border-box",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          scrollSnapAlign: "center",
-          ...entryContainerStyleEffective,
-          marginTop: isFirst
-            ? containerHeight / 2 - entryHeight / 2
-            : undefined,
-          marginBottom: isLast
-            ? containerHeight / 2 - entryHeight / 2
-            : undefined,
+          opacity: inView ? 1 : 0.35,
+          transition: "opacity 0.5s",
+          zIndex: 2,
+          ...entryContentStyleEffective,
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            opacity: inView ? 1 : 0.35,
-            transition: "opacity 0.5s",
-            zIndex: 2,
-            ...entryContentStyleEffective,
-          }}
-        >
-          {unit === "metric" ? metricValueFormatted : imperialValueFormatted}
-          <SecondaryListEntry>
-            {unit === "metric" ? imperialValueFormatted : metricValueFormatted}
-          </SecondaryListEntry>
-        </div>
+        {unit === "metric" ? metricValueFormatted : imperialValueFormatted}
+        <SecondaryListEntry>
+          {unit === "metric" ? imperialValueFormatted : metricValueFormatted}
+        </SecondaryListEntry>
       </div>
-    );
-  },
-  (prevProps, nextProps) => {
-    const { parentRef: p, ...prefRest } = prevProps;
-    const { parentRef: n, ...nextRest } = prevProps;
-    return JSON.stringify(prefRest) === JSON.stringify(nextRest);
-  },
-);
+    </div>
+  );
+};
