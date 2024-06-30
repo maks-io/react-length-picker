4c28bb03c93e06b11c3fb523965b8998be9b6e37
Markus Kurzmann <markus@kurzmann.io>
2024-06-30 19:37:30 +0200

    Export conversion functions

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import { LengthPicker } from "$/components/LengthPicker";
 import { Unit } from "$/types/Unit";
+import { imperialToMetric } from "$/convert/imperialToMetric";
+import { inchesToFeetAndInches } from "$/convert/inchesToFeetAndInches";
+import { metricToImperial } from "$/convert/metricToImperial";
 
-export { LengthPicker };
+export {
+  LengthPicker,
+  imperialToMetric,
+  inchesToFeetAndInches,
+  metricToImperial,
+};
 export type { Unit };
