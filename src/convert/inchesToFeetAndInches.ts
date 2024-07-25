7b7bfde3f0fe6ea1beff8394a63060c766dee62f
Markus Kurzmann <markus@kurzmann.io>
2024-07-25 23:21:56 +0200

    Fix inchesToFeetAndInches for negative values

--- src/convert/inchesToFeetAndInches.ts
+++ src/convert/inchesToFeetAndInches.ts
@@ -4,6 +4,14 @@ export const inchesToFeetAndInches = (
   inches: number,
   roundInches = false,
 ): FeetAndInches => {
+  if (inches < 0) {
+    const positiveValue: FeetAndInches = inchesToFeetAndInches(
+      inches * -1,
+      roundInches,
+    );
+    return { feet: positiveValue.feet * -1, inches: positiveValue.inches * -1 };
+  }
+
   const usedInches = roundInches ? Math.round(inches) : inches;
   return {
     feet: Math.floor(usedInches / 12),
